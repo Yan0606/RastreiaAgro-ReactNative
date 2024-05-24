54992b7feb4dd6473d654f6bb4855f440420f729
yan0606 <yan060604@gmail.com>
2024-05-24 17:54:00 -0300

    :sparkles: make safeArea in the app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Logo from './src/components/logo';
@@ -10,25 +10,35 @@ import Login from './src/components/pages/login/index';
 import Cadastro from './src/components/pages/cadastro/index';
 import CadastroPropriedade from './src/components/pages/cadastroPropriedade/index';
 import Obrigado from './src/components/pages/obrigado';
+import Menu from './src/components/pages/menu/menu';
+import { SafeAreaProvider, SafeAreaInsetsContext } from 'react-native-safe-area-context';
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Logo />
-      <Title />
-      <Main />
-    </View>
+    <SafeAreaInsetsContext.Consumer>
+      {insets => <View style={{ paddingTop: insets.top, ...styles.container }}>
+        <Logo />
+        <Title />
+        <Main />
+      </View >}
+    </SafeAreaInsetsContext.Consumer>
   );
 }
 
 function LoginScreen({ navigation }) {
   return (
-    <Login />
+    <SafeAreaInsetsContext.Consumer>
+      {insets => <Login style={{ paddingTop: insets.top }} />}
+    </SafeAreaInsetsContext.Consumer >
   );
 }
 function CadastroScreen({ navigation }) {
   return (
-    <Cadastro />
+    <SafeAreaInsetsContext.Consumer>
+      {insets => <View style={{ paddingTop: insets.top, ...styles.container }}>
+        <Cadastro />
+      </View >}
+    </SafeAreaInsetsContext.Consumer>
   );
 }
 
@@ -44,20 +54,30 @@ function ObrigadoScreen({ navigation }) {
   );
 }
 
+function MenuScreen({ navigation }) {
+  return (
+    <Menu />
+  );
+}
+
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Cadastro" component={CadastroScreen} />
-        <Stack.Screen name="CadastroPropriedade" component={CadastroPropriedadeScreen} />
-        <Stack.Screen name="Obrigado" component={ObrigadoScreen} />
-        {/* novas telas adicionar aqui */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Cadastro" component={CadastroScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="CadastroPropriedade" component={CadastroPropriedadeScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Obrigado" component={ObrigadoScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Menu" component={MenuScreen} />
+          {/* novas telas adicionar aqui */}
+
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
