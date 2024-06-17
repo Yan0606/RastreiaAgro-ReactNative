7f126b2d27e6210bc2a0f29728dad32f006d4d29
yan0606 <yan060604@gmail.com>
2024-06-17 11:37:16 -0300

    feat: ✨  rota para menu Configuração

--- src/components/pages/menu/header/config/config.js
+++ src/components/pages/menu/header/config/config.js
@@ -1,13 +1,21 @@
 import React from "react"
-import { View, Image } from "react-native"
+import { View, Image, TouchableOpacity} from "react-native"
 import styles from "./style"
+import { useNavigation } from "@react-navigation/native";
+
 
 export default function Config() {
+    const navigation = useNavigation();
+    const menuConfiguracao = () => {
+        navigation.navigate('MenuConfiguracao');
+    }
     return (
         <View>
-            <Image style={styles.logo}
-                source={require('../../../../../img/config.png')}
-            />
+            <TouchableOpacity onPress={() => { menuConfiguracao() }}>
+                <Image style={styles.logo}
+                    source={require('../../../../../img/config.png')}
+                />
+            </TouchableOpacity>
         </View>
     )
 }
